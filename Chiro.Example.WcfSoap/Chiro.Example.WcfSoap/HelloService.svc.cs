f9db222ced104c933a300855b80f2b2c52d2be3c
Johan Vervloet <johanv@johanv.org>
2014-01-06 17:28:17 +0100

    Initial commit.
    Just an example service. Works out of the box as SOAP 1.1.

---
+++ Chiro.Example.WcfSoap/Chiro.Example.WcfSoap/HelloService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Chiro.Example.WcfSoap
+{
+    public class HelloService : IHelloService
+    {
+        public string Hello(string name)
+        {
+            return string.Format("Hello {0}!", name);
+        }
+    }
+}
---
+++ Chiro.Example.WcfSoap/Chiro.Example.WcfSoap/IHelloService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Chiro.Example.WcfSoap
+{
+    [ServiceContract]
+    public interface IHelloService
+    {
+        /// <summary>
+        /// Say hello to person with given <paramref name="name"/>.
+        /// </summary>
+        /// <param name="name">Name of person to say hello to.</param>
+        /// <returns>Hello-message</returns>
+        [OperationContract]
+        string Hello(string name);
+    }
+}
